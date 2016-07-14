35939173a197b6be0a2f0462126826ba4ebd5e08
LopsanAMO <lmolinao1200@alumno.ipn.mx>
2016-07-14 18:08:59 -0500

    Almost ready

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,31 @@
-
- (function (){
+(function ($scope,$http){
      var app = angular.module('usuario_serv', []);
-        app.controller('serviciosController', function($scope, $http) {
-         var nombre = [];
-         nombre.push(extraer($http));
-        console.log("hola" + nombre[0]);
-         this.color = {
-            name : nombres.data.name,
-         };
-        });
+        app.controller('serviciosController', function($scope,$http) {
+        this.nombres = [];
+        this.thing = [];
+        this.thing = $http.get("/API/colores_list/")
+          .then(function(response) {
+              var nombres = [];
+              console.log(response.data.length);
+              cantidad = response.data.length;
+              for (i = 0; i < cantidad; i++) {
+                  nombres.push(response.data[i].name);
+              }
+              //console.log(hola);
+              for (j = 0; j < cantidad; j++) {
+                  console.log(nombres[j]);
+              }
+              console.log(nombres);
+              return nombres;
+          });
+          console.log("algo");
+          console.log(this.thing);
+        function extraerx2(){
 
-        function extraer($http) {
+            }
 
-              $http.get("/API/colores_list/")
-            .then(function(response) {
-                var nombres = [];
-                console.log(response.data.length);
-                cantidad = response.data.length;
-                for (i = 0; i < cantidad; i++) {
-                    nombres.push(response.data[i].name);
-                }
-                //console.log(hola);
-                for (j = 0; j < cantidad; j++) {
-                    console.log(nombres[j]);
-                }
-                console.log(nombres);
-                return nombres;
-            });
-        }
-        function things(lista){
-            console.log(lista);
-            return lista;
-        }
+        this.nombres=extraerx2();
+        console.log("hola");
+        console.log(this.nombres);
+      });
   })();
---
+++ static/js/app2.js
@@ -0,0 +1,35 @@
+
+ (function (){
+     var app = angular.module('usuario_serv', []);
+        app.controller('serviciosController', function($scope, $http) {
+         var nombre = [];
+         nombre.push(extraer($http));
+        console.log("hola" + nombre[0]);
+         this.color = {
+            name : nombre.data.name,
+         };
+        });
+
+        function extraer($http) {
+
+              $http.get("/API/colores_list/")
+            .then(function(response) {
+                var nombres = [];
+                console.log(response.data.length);
+                cantidad = response.data.length;
+                for (i = 0; i < cantidad; i++) {
+                    nombres.push(response.data[i].name);
+                }
+                //console.log(hola);
+                for (j = 0; j < cantidad; j++) {
+                    console.log(nombres[j]);
+                }
+                console.log(nombres);
+                return nombres;
+            });
+        }
+        function things(lista){
+            console.log(lista);
+            return lista;
+        }
+  })();
