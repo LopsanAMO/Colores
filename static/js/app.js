40739d61b24c7253f70cb736d58c64a7af1db72e
LopsanAMO <lmolinao1200@alumno.ipn.mx>
2016-07-15 01:49:55 -0500

    termine mi ejemplo de consumo de APIs con Angular

--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,28 @@
 (function ($scope,$http){
      var app = angular.module('usuario_serv', []);
         app.controller('serviciosController', function($scope,$http) {
-        this.nombres = [];
-        this.thing = [];
-        this.thing = $http.get("/API/colores_list/")
+
+        this.diccs = $http.get("/API/colores_list/?format=json")
           .then(function(response) {
-              var nombres = [];
-              console.log(response.data.length);
+              var diccionarios = [];
+              var dicaux = {};
               cantidad = response.data.length;
               for (i = 0; i < cantidad; i++) {
-                  nombres.push(response.data[i].name);
+                  dicaux.nombre = response.data[i].name;
+                  dicaux.descripcion = response.data[i].description;
+                  dicaux.hexadeciamal = response.data[i].hexadecimal;
+                  dicaux.rojo = response.data[i].red;
+                  dicaux.verde = response.data[i].green;
+                  dicaux.azul = response.data[i].blue;
+
+
+                  diccionarios[i]=dicaux;
+                  dicaux = {};
               }
-              //console.log(hola);
-              for (j = 0; j < cantidad; j++) {
-                  console.log(nombres[j]);
-              }
-              console.log(nombres);
-              return nombres;
+              console.log(diccionarios[0]);
+              return diccionarios;
           });
-          console.log("algo");
-          console.log(this.thing);
-        function extraerx2(){
-
-            }
-
-        this.nombres=extraerx2();
-        console.log("hola");
-        console.log(this.nombres);
+          console.log("hola");
+          console.log(this.diccs.$$state.value);
       });
   })();
