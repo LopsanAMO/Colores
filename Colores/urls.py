a501fad2f6e7eb879a59bf1a5f2deeb67acdce71
LopsanAMO <lmolinao1200@alumno.ipn.mx>
2016-07-17 23:22:21 -0500

    Correccion de rutas

--- Colores/urls.py
+++ Colores/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from App.api import urls as urlsAPI
 from App import urls as ur
+from App.views import Home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^API/', include(urlsAPI, namespace='API')),
-    url(r'^App/', include(ur, namespace='app'))
+    url(r'^App/', include(ur, namespace='app')),
+    url(r'^$', Home.as_view()),
 ]
